ae57e3e27328c989a790585716b05aabb4940d91
Blen Wondimagegn <mekuriablen@gmail.com>
2020-08-20 01:38:35 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,8 +5,7 @@ def oxford_comma(array)
     array.join(' and ')
   else array.length == 3
     array.join(',')
-   arr= array[-2]<<('and')
-   array
-   
+   arr = array.pop
+   array.join(", ") << ", and #{lastStr}"
   end
 end
